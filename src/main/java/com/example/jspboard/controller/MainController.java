83f55d4f6582603dc3cc8e0b4523a90efc0709a3
woojin Kim <woojkk@naver.com>
2023-11-15 23:17:41 +0900

    [feat] - controller 분리, logback-spring.xml생성

--- src/main/java/com/example/jspboard/controller/MainController.java
+++ src/main/java/com/example/jspboard/controller/MainController.java
@@ -1,22 +1,10 @@
 package com.example.jspboard.controller;
 
-import com.example.jspboard.service.StudyService;
-import com.example.jspboard.vo.StudyVO;
-import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@Slf4j
 public class MainController {
-
-  private final StudyService studyService;
-
-  public MainController(StudyService studyService) {
-    this.studyService = studyService;
-  }
 
   @GetMapping("/")
   public String doHome() {
@@ -31,22 +19,5 @@ public class MainController {
   @GetMapping("/login")
   public String doLogin() {
     return "/login";
-  }
-  @GetMapping("/studyReg")
-  public String doStudy_reg(Model model) {
-
-    List<StudyVO> list = studyService.doStudyList();
-
-    log.info("vo_studey");
-    for (StudyVO studyVO : list) {
-      log.info(studyVO.getKeyId());
-      log.info(studyVO.getStudyDay());
-      log.info(studyVO.getContents());
-      log.info(studyVO.getRegDay());
-    }
-
-    model.addAttribute("list", list);
-
-    return "/studyReg";
   }
 }
--- src/main/webapp/WEB-INF/views/comm/header.jsp
+++ src/main/webapp/WEB-INF/views/comm/header.jsp
@@ -11,7 +11,7 @@
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="/" class="nav-link px-2 text-secondary">홈</a></li>
-                <li><a href="/study_reg" class="nav-link px-2 text-white">공부기록</a></li>
+                <li><a href="/studyReg" class="nav-link px-2 text-white">공부기록</a></li>
             </ul>
 
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
